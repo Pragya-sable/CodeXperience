f7ffd95ecf31fdc53cef84c39f512559d0166bc2
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-07 12:19:21 +0530

    abc

---
+++ Addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Addition{
+public static void main(String[]args){
+Scanner kb=new Scanner(System.in);
+int a,b;
+a=kb.nextInt();
+b=kb.nextInt();
+int add=0;
+add=a+b;
+System.out.println("Addition is "+add);
+}
+}
