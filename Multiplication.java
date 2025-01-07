e8e7c6747b6a31ae4eb724b24b2b8e25aad1fa99
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-07 12:30:21 +0530

    added

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Multiplication{
+public static void main(String[]args){
+Scanner kb=new Scanner(System.in);
+int a,b;
+a=kb.nextInt();
+b=kb.nextInt();
+int mul=0;
+mul=a*b;
+System.out.println("Multiplication is "+mul);
+}
+}
