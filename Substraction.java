3c2b71a3df44e2a0bb4a564dabdbf71908712397
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-07 12:26:12 +0530

    added

---
+++ Substraction.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Substraction{
+public static void main(String[]args){
+Scanner kb=new Scanner(System.in);
+int a,b;
+a=kb.nextInt();
+b=kb.nextInt();
+int sub=0;
+sub=a-b;
+System.out.println("Substraction is "+sub);
+}
+}
