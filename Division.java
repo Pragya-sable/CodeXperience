5066b9cb278ae7ea19a7fa55a3234eecbe81667e
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-07 12:35:24 +0530

    added

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Division{
+public static void main(String[]args){
+Scanner kb=new Scanner(System.in);
+int a,b;
+a=kb.nextInt();
+b=kb.nextInt();
+int div=0;
+div=a/b;
+System.out.println("Division is "+div);
+}
+}
